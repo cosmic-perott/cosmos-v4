ec39c0ca43bebeb0c601e0562029534a7c8fa2b0
cosmic_perott <142322366+cosmic-perott@users.noreply.github.com>
2026-05-28 15:29:03 +0900

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,11 +1,15 @@
-
+main() {
+ https://github.com/cosmic-perott/cosmos-v4.git
+}
 terminal_1() {
+  cd listener
   node server.js
 }
 
 terminal_1
 
 terminal_2() {
+  cd listener
   ngrok http 3000
 }
 
